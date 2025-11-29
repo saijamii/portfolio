f927889535bf392bbed23ad9eb244b0600e3662d
saijamii <saijami38@gmail.com>
2025-11-29 20:05:37 +0530

    Separator styles

--- src/components/ui/Separator.jsx
+++ src/components/ui/Separator.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { cn } from "../../lib/utils";
 
-function Separator({ className }) {
+export function Separator({ className }) {
     return (
         <div
             className={cn(
-                "relative flex h-8 w-full border-x border-edge",
-                "before:absolute before:-left-[100vw] before:z-[-1] before:h-8 before:w-[200vw]",
-                "before:bg-[repeating-linear-gradient(315deg,var(--pattern-foreground)_0,var(--pattern-foreground)_1px,transparent_0,transparent_50%)] before:bg-size-[10px_10px] pattern-edge",
+                "relative flex h-8 w-full border-x border-edge pattern-edge",
+                "before:content-[''] before:absolute before:-left-[100vw] before:top-0",
+                "before:z-[10] before:h-8 before:w-[200vw]",
+                "before:[background-size:10px_10px]",
+                "before:bg-[repeating-linear-gradient(315deg,var(--pattern-foreground)_0,var(--pattern-foreground)_1px,transparent_0,transparent_50%)]",
                 className
             )}
         />
     );
 }
-
-export { Separator };
